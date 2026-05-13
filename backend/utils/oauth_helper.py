37fa001587601711de45f87b919fc3d612086645
nandinis898 <nandinis898@gmail.com>
2026-05-13 13:29:47 +0530

    added credentials.json

--- backend/utils/oauth_helper.py
+++ backend/utils/oauth_helper.py
@@ -1,11 +1,20 @@
+import os
 from google_auth_oauthlib.flow import Flow
 
-CLIENT_SECRETS_FILE = "credentials.json"
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+BASE_DIR = os.path.dirname(__file__)
+
+CLIENT_SECRETS_FILE = os.path.join(
+    BASE_DIR,
+    "credentials.json"
+)
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly"
+]
 
 def get_flow():
     return Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE,
         scopes=SCOPES,
-        redirect_uri ="https://pryoport-backend.onrender.com/oauth2callback"
+        redirect_uri="https://pryoport-backend.onrender.com/oauth2callback"
     )
